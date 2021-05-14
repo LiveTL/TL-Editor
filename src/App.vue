0dd66ab8daf37b9eb46d9e9ed8a5a44c2d8f8f33
Kento Nishi <kento24gs@outlook.com>
2021-05-14 01:01:19 -0700

    Working play pause button

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,46 @@
       app
       color="primary"
     >
-      <div style="width: 100%; color: white; text-align: right; font-size: 2rem;">
-        {{timestamp}}
+      <div class="wrapper">
+        <v-text-field
+          label="Hour"
+          filled dark
+          :rules="[validTimestamp]"
+          v-model="timestamp[0]"
+          @change="timeChanged()"
+          @focus="player.pauseVideo()"
+        ></v-text-field>
+        <v-text-field
+          label="Minute"
+          filled dark
+          :rules="[validTimestamp]"
+          v-model="timestamp[1]"
+          @change="timeChanged()"
+          @focus="player.pauseVideo()"
+        ></v-text-field>
+        <v-text-field
+          label="Second"
+          filled dark
+          :rules="[validTimestamp]"
+          v-model="timestamp[2]"
+          @change="timeChanged()"
+          @focus="player.pauseVideo()"
+        ></v-text-field>
+        <v-btn
+          class="mx-2"
+          fab
+          dark
+          small
+          color="cyan"
+          @click="togglePlay()"
+        >
+          <v-icon dark v-if="player && player.getPlayerState() == 1">
+            mdi-pause
+          </v-icon>
+          <v-icon dark v-else>
+            mdi-play
+          </v-icon>
+        </v-btn>
       </div>
     </v-app-bar>
 
@@ -25,7 +63,7 @@ export default {
   },
   data: () => ({
     player: null,
-    timestamp: '00:00:00'
+    timestamp: [0, 0, 0]
   }),
   mounted() {
     window.addEventListener('message', packet => {
@@ -33,9 +71,21 @@ export default {
       if (data.event === 'infoDelivery') {
         this.timestamp = new Date(
           parseFloat(data.info.currentTime) * 1000
-        ).toISOString().substr(11, 8);
+        ).toISOString().substr(11, 8).split(':').map(i => parseInt(i));
       }
     });
+  },
+  methods: {
+    validTimestamp(val) {
+      return val >= 0 && val <= 60;
+    },
+    timeChanged() {
+      this.player.seekTo(((this.timestamp[0] * 60) + this.timestamp[1]) * 60 + this.timestamp[2]);
+    },
+    togglePlay() {
+      if (this.player.getPlayerState() === 1) this.player.pauseVideo();
+      else this.player.playVideo();
+    }
   }
 };
 </script>
@@ -46,5 +96,22 @@ html {
 }
 .v-main__wrap>div {
   height: 100%;
+}
+.v-input, .v-btn {
+  margin: 10px !important;
+  margin-top: 35px !important
+}
+.v-btn {
+  transform: translateY(10px);
+}
+.wrapper {
+  width: 100%;
+  color: white;
+  text-align: right;
+  font-size: 2rem;
+  display: flex;
+}
+.v-app-bar {
+  overflow-y: hidden;
 }
 </style>
