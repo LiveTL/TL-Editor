3b1c4b512b8fded7cff6dd2a895741c68000ca4c
Grumpy Bear <grumpybear4257@gmail.com>
2021-09-02 22:11:23 -0600

    created deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "pulling changes and building"
+git pull
+yarn build
+
+echo "removing old version and copying new"
+sudo rm -rf /opt/livetl-editor/*
+sudo cp -r ./dist/* /opt/livetl-editor/
+sudo chown -R www-data:www-data /opt/livetl-docs/
+
+echo "done"
